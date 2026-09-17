17e3806c452d909e902243542235f8c1befc940c
Kim Mun Restaurant <kimmunrestaurant@users.noreply.github.com>
2026-09-17 16:42:46 -0500

    Group egg dishes by protein

---
+++ menu-groups.js
@@ -0,0 +1,14 @@
+(() => {
+  const menu = window.MAIN_MENU;
+  if (!menu || !menu.EGG) return;
+
+  const eggItems = new Map(menu.EGG.map(item => [item.code, item]));
+  const move = (category, codes) => {
+    menu[category].push(...codes.map(code => eggItems.get(code)).filter(Boolean));
+  };
+
+  move('CHOP SUEY', ['43', '43A', '46A']);
+  move('PRAWN', ['45', '46']);
+  move('CHICKEN', ['44', '47']);
+  delete menu.EGG;
+})();
